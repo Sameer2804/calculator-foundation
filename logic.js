3995042957a1312e709404250a9b57fd8815affa
Sameer <sameer.ahmed1@kpmg.co.uk>
2023-12-21 11:35:55 +0000

    created the calculator object

--- logic.js
+++ logic.js
@@ -1,1 +1,19 @@
-console.log('hello')
+function Calculator(){
+    const obj = {};
+    this.method = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '*': (a, b) => a * b,
+        '/': (a, b) => a / b,
+    };
+
+    this.calculate = function(numOne, numTwo, operator){
+        return this.method[operator](numOne, numTwo)
+    }
+}
+
+const calc = new Calculator();
+console.log(calc.calculate(5, 6, '+'));
+console.log(calc.calculate(6, 5, '-'));
+console.log(calc.calculate(5, 6, '*'));
+console.log(calc.calculate(12, 7, '/'));
